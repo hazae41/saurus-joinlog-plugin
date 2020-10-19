5203161d2a10c470da0bba778ad5d99de1a94d56
hazae41 <hazae41@gmail.com>
2020-10-19 12:17:12 +0000

    added server logs

--- mod.ts
+++ mod.ts
@@ -1,12 +1,11 @@
 import type { Player } from "saurus/player.ts";
 import type { Server } from "saurus/server.ts";
 
-const config = {
-  join: (p: Player) => `${p.name} joined the game`,
-  quit: (p: Player) => `${p.name} left the game`
-}
-
-export class JoinLog {
+export class PlayerJoinLog {
+  config = {
+    join: (p: Player) => `${p.name} joined the game`,
+    quit: (p: Player) => `${p.name} left the game`
+  }
 
   /**
    * Plugin that logs when a player join/leave a given server.
@@ -14,11 +13,31 @@ export class JoinLog {
    */
   constructor(readonly server: Server) {
     const offjoin = server.players.on(["join"],
-      (p) => console.log(config.join(p)))
+      (p) => console.log(this.config.join(p)))
 
     const offquit = server.players.on(["quit"],
-      (p) => console.log(config.quit(p)))
+      (p) => console.log(this.config.quit(p)))
 
     server.once(["close"], offjoin, offquit)
+  }
+}
+
+export class ServerJoinLog {
+  config = {
+    connect: (server: Server) =>
+      `${server.name} connected`,
+    disconnect: (server: Server, reason: string) =>
+      `${server.name} disconnected (${reason})`,
+  }
+
+  /**
+   * Server plugin that logs when the server is connected/disconnected.
+   * @param server Server you want to activate the plugin on
+   */
+  constructor(server: Server) {
+    console.log(this.config.connect(server))
+
+    server.once(["close"], (e) =>
+      console.log(this.config.disconnect(server, e.reason)))
   }
 }
